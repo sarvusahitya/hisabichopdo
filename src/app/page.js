de8b48c345e34ea95107084ffa397ff3d9817184
sarvusahitya <sarvusahitya@gmail.com>
2024-06-08 22:20:41 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex  flex-col items-center justify-between p-24">
       <div className=" grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <a
-          href="#"
+          href="https://www.google.co.in/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"

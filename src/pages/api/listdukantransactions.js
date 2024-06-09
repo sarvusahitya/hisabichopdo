021ab619ce1ce065063642935885f5406e7afb5e
sarvusahitya <sarvusahitya@gmail.com>
2024-06-09 18:25:36 +0530

    update

---
+++ src/pages/api/listdukantransactions.js
@@ -0,0 +1,33 @@
+import connectToDatabase from "@/app/lib/db";
+import TransactionModel from "@/app/models/TransactionsModel"; // Update import
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Only GET requests allowed" });
+  }
+
+  try {
+    // Connect to the database
+    await connectToDatabase();
+
+    // Get the first and last day of the current month
+    const now = new Date();
+    const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+    const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+
+    // Find transactions within the current month
+    const transactions = await TransactionModel.find({
+      date: {
+        $gte: firstDay,
+        $lt: lastDay,
+      },
+      type: "credit",
+    });
+
+    // Respond with the transactions
+    return res.status(200).json(transactions);
+  } catch (error) {
+    console.error("Error saving entry:", error);
+    return res.status(500).json({ message: "An error occurred" });
+  }
+}

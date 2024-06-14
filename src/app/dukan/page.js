8a1d235cdaf9559ab20514ec079a7bf0b85a6f3a
sarvusahitya <sarvusahitya@gmail.com>
2024-06-15 00:32:04 +0530

    update

--- src/app/dukan/page.js
+++ src/app/dukan/page.js
@@ -40,6 +40,18 @@ export default function DukanLandingPage() {
           </span>
         </h2>
       </Link>
+      <Link
+        href={"/dukan/dukanvendortransactions"}
+        className="group  rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      >
+        {" "}
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+          દુકાનન જમા-ઉધાર{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+      </Link>
     </main>
   );
 }
